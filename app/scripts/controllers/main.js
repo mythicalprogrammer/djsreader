2f104191c1c69b1b62d10ec2bd8a982073f1e636
Anthony Doan <mythicalprogrammer@gmail.com>
2013-05-19 20:33:18 -0700

    finish part 4 of the tutorial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,24 +1,47 @@
 'use strict';
 
 angular.module('djsreaderApp')
-  .controller('MainCtrl', function ($scope, $http) {
-      var query = "select * from xml where url='http://dailyjs.com/atom.xml' and itemPath='feed.entry'"
-	  var options = "&format=json&diagnostics=true&callback=JSON_CALLBACK";
+	.controller('MainCtrl', function ($scope, $http, $timeout) {
+		$scope.refreshInterval = 60;
 
-	  query = encodeURI(query+options);
+		$scope.feeds = [{
+			url: 'http://dailyjs.com/atom.xml'
+	  	}];
 
-	  var yql = "http://query.yahooapis.com/v1/public/yql?q=";
-	  var url = yql+query;
+		$scope.fetchFeed = function(feed) {
+			feed.items = [];
+	  
+      		var query = "select * from xml where url='"+feed.url+"' and itemPath='feed.entry'";
+	  		var options = "&format=json&diagnostics=true&callback=JSON_CALLBACK";
 
-	  $http.jsonp(url)
-	  	.success(function(data, status, headers, config) {
-			$scope.feed = {
-				title: 'DailyJS',
-				items: data.query.results.entry
-			};
-		})
-		.error(function(data, status, headers, config) {
-			console.error('Error fetching feed:', data);
-		});
+	  		query = encodeURI(query+options);
+
+	  		var yql = "http://query.yahooapis.com/v1/public/yql?q=";
+	  		var url = yql+query;
+
+	  		$http.jsonp(url)
+	  			.success(function(data, status, headers, config) {
+					if (data.query.results) {
+						feed.items = data.query.results.entry;
+					}
+				})
+				.error(function(data, status, headers, config) {
+					console.error('Error fetching feed:', data);
+				});
+
+			$timeout(function() { $scope.fetchFeed(feed);}, $scope.refreshInterval * 1000);
+		};
+
+		$scope.addFeed = function(feed) {
+			$scope.feeds.push(feed);
+			$scope.fetchFeed(feed);
+			$scope.newFeed = {};
+		};
+
+		$scope.deleteFeed = function(feed) {
+			$scope.feeds.splice($scope.feeds.indexOf(feed), 1);
+		};
+
+
 
   });
